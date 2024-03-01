ac5491ecf0d3d816ae4bdb9d995db50ae21359f2
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-03-01 10:48:41 +0100

    adjusted cors

--- server.js
+++ server.js
@@ -20,16 +20,17 @@ connectDB();
 // Parse JSON request body
 app.use(express.json());
 app.use(cors({
-    origin: allowOrigins
+    origin: function (origin, callback) {
+        if (!origin || allowOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://main--tod0ify-app.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
 
 app.get('/', (req, res) => {
     res.send("server is running");
