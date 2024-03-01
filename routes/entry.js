345a702c3e517e04a56fc0074c7f2e75fb4d1460
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-03-01 17:29:38 +0100

    new entry points to play with

--- routes/entry.js
+++ routes/entry.js
@@ -40,4 +40,40 @@ router.get('/todos', async (req, res) => {
     }
 })
 
+router.patch('/done', async (req, res) => {
+  console.log(req.body)
+  try{
+    const { taskId } = req.body;
+
+    const updatedTodo = await Todo.findByIdAndUpdate(taskId, {
+      isDone: true,
+      completed: new Date()
+    }, { new: true });
+
+    if(!updatedTodo){
+      return res.status(404).json({ message: 'Task not found'});
+    }
+    res.status(200).json({ message: 'Task marked as done successfully'});
+  } catch(error) {
+    console.error('Error setting task to done:', error);
+    res.status(500).json({ message: 'Internal server error '})
+  }
+})
+
+router.delete('/delete/:taskId', async(req, res) => {
+  const { taskId } = req.params;
+  console.log("taskId: ", taskId);
+  try{
+    const entryToDelete = await Todo.findByIdAndDelete(taskId);
+    if(!entryToDelete){
+      return res.status(404).json({ message: 'Entry not found'})
+    }
+
+    return res.status(200).json({ message: 'successfully deleted entry! '})
+  }catch(error) {
+    console.error("could not delete entry");
+    return res.status(500).json({ message: 'Internal server error'})
+  }
+})
+
 module.exports = router;
