f963bced079939f7b89c1106b5b91379a9ad6ba1
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-03-05 08:02:53 +0100

    changed route

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const { authenticate } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/profile', authenticate, (req, res) => {
+router.get('/auth', authenticate, (req, res) => {
   res.json({ message: `Welcome ${req.user.username}` });
 });
 
